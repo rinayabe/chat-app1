bb09a61d9b4cbc3c90c7e710b370d8a200c186d4
rinayabe <se140663@yahoo.co.jp>
2020-09-11 16:09:57 +0900

    あ

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,10 +2,6 @@ class RoomsController < ApplicationController
 
   def index
   end
-  
-  def new
-    @room = Room.new
-  end
 
   def create
     @room = Room.new(room_params)
@@ -16,6 +12,13 @@ class RoomsController < ApplicationController
     end
     
   end
+
+  def destroy
+    room = Room.find(params[:id])
+    room.destroy
+    redirect_to root_path
+  end
+
   private
 
   def room_params
